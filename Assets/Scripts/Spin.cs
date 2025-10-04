953f20e26c79b76f5c12a7284a26d6accb985627
dv.barashkov <dv.barashkov@tensor.ru>
2025-10-04 21:15:20 +0300

    class names

--- Assets/Scripts/Spin.cs
+++ Assets/Scripts/Spin.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class Spin : MonoBehaviour
 {
-    [SerializeField] private float _spinSpeed;
+    [SerializeField] private float _speed;
 
     private void Update()
     {
-        transform.Rotate(Vector3.up * _spinSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.up * _speed * Time.deltaTime);
     }
 }
